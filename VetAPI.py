dd875b1ec3a80df5067c1b00a09e451279628946
MatthewBieda <matthew.bieda@gmail.com>
2022-04-27 13:18:13 +0100

    added more API functionality

--- VetAPI.py
+++ VetAPI.py
@@ -46,8 +46,11 @@ print(Animals_JSON)
 
 @app.route('/', methods=['GET']) #tell which HTTP method we are using (GET) and what route (extra bit of the URL) this method will be activated on.  In this case nothing and so home
 def home():
-    return "<h1>Welcome to our virtual veterinary practice</h1><p>See information on our <a href='/api/animals')>animals</a></p>"
-
+    return (
+    "<h1>Welcome to our virtual veterinary practice API</h1><p>See information on our <a href='/api/animals')>animals</a> and <a href='/api/customers')>customers</a>.</p>"
+    "<p>We provide a standard REST API.</p>"
+    "<p>Animals can also be <a href='/api/animals/agequery?age=10')>queried via age.</a></p>"
+    )
 
 # A route to return all of the available entries in our collection of pet owners.
 @app.route('/api/customers/', methods=['GET'])
@@ -69,15 +72,17 @@ def get_owner_by_id(id):
     # Python dictionaries to the JSON format.
     return jsonify(results)
 
+
 @app.route('/api/')
 def redirect():
-    return("<h1>Animals / Customers</h1>")
+    return("<h1><a href='/api/animals')>Animals</a> <br><br> <a href='/api/customers')>Customers</a></h1>")
 
+#List view for all animals
 @app.route('/api/animals/', methods=['GET'])
 def animals_api_all():
     return jsonify(Animals_JSON)
 
-
+#Detail view for animals
 @app.route('/api/animals/<int:id>', methods=['GET'])
 def animals_api_unique(id):
     if id >= len(Animals_JSON):
@@ -88,5 +93,32 @@ def animals_api_unique(id):
     available_attributes = vars(object)
     #Vars calls __dict__ method on our object under the hood
     return available_attributes
+
+
+#Adding a route that enables searching for animals by their age
+@app.route('/api/animals/agequery', methods=['GET'])
+def animals_by_age():
+    # Check if an age was provided as part of the URL.
+    # If age is provided, assign it to a variable.
+    # If no age is provided, display an error in the browser.
+    if 'age' in request.args:
+        age = int(request.args['age'])
+    else:
+        return "Error: Invalid query."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested age.
+    for animal in Animals:
+        if 'age' in vars(animal):
+            if animal.age == age:
+                results.append(str(animal))
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    if len(results) == 0:
+        return("<h1>No animals found which match that query</h1>")
+    return jsonify(results)
+
 
 app.run()
